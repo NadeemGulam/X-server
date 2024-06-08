33760a8540adb5e36458433113c56682f2d88914
Nadeem Gulam <68492875+NadeemGulam@users.noreply.github.com>
2024-06-08 17:17:40 +0530

    added the Health route

--- src/app/index.ts
+++ src/app/index.ts
@@ -15,6 +15,10 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json())
 
+app.get("/", (req, res) => {
+    res.status(200).json({ message: "Everything is good" })
+})
+
 const graphqlServer = new ApolloServer<GraphqlContext>({
 
     typeDefs: `
