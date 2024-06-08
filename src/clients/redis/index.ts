41cbed3fd6225477043b45205936fcd4a8231e9b
Nadeem Gulam <68492875+NadeemGulam@users.noreply.github.com>
2024-06-08 16:26:27 +0530

    setting the env file

--- src/clients/redis/index.ts
+++ src/clients/redis/index.ts
@@ -1,6 +1,6 @@
 import Redis from "ioredis"
 
 export const redisClient = new Redis(
-    "rediss://default:AdOWAAIncDExOGYwYjU5MTA4ZjU0MDg2YjAxNGY4MWE3MTE4NTgxNnAxNTQxNjY@grand-tuna-54166.upstash.io:6379"
+    process.env.REDIS_URL as string
 );
 
