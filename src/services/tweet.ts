d1bb4f703bd10c212b817d605b847ffa7a167c89
Nadeem Gulam <68492875+NadeemGulam@users.noreply.github.com>
2024-06-08 12:21:05 +0530

    Bug Fix : Tweets reload after new Tweet ( call : GetAllTweets) was not wrking as intended

--- src/services/tweet.ts
+++ src/services/tweet.ts
@@ -10,7 +10,7 @@ export interface CreateTweetPayload {
 class TweetService {
     public static async createTweet(data: CreateTweetPayload) {
         const rateLimitFlag = await redisClient.get(`RATE_LIMIT:TWEET:${data.userId}`)
-        if(rateLimitFlag) {
+        if (rateLimitFlag) {
             throw new Error("Please wait ...")
         }
         const tweet = await prismaClient.tweet.create({
@@ -19,20 +19,27 @@ class TweetService {
                 imageURL: data.imageURL,
                 author: { connect: { id: data.userId } }
             }
-        })
-        await redisClient.setex(`RATE_LIMIT:TWEET:${data.userId}`, 10, 1)
-        await redisClient.del('ALL_TWEETS')
+        });
+        await redisClient.setex(`RATE_LIMIT:TWEET:${data.userId}`, 10, 1);
+        await redisClient.del('ALL_TWEETS');
         return tweet;
     }
 
     public static async getAllTweets() {
-        const cachedTweets = await redisClient.get('ALL_TWEETS')
+        const cachedTweets = await redisClient.get('ALL_TWEETS');
 
         if (cachedTweets) return JSON.parse(cachedTweets);
-        const tweets = await prismaClient.tweet.findMany({ orderBy: { createdAt: "desc" } })
+        
+        const tweets = await prismaClient.tweet.findMany({
+            include: {
+                author: true
+            },
+            orderBy: { createdAt: "desc" }
+        });
+        
         await redisClient.set('ALL_TWEETS', JSON.stringify(tweets));
+        return tweets;
     }
 }
-
 
 export default TweetService;
